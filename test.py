d57979dc2c3f8b2fd3adf92246c712a82a97dda5
David Sherlock <davidsherlock163@hotmail.com>
2019-10-31 11:41:09 +0000

    put everything in 1 file

--- test.py
+++ test.py
@@ -5,43 +5,56 @@ import re
 
 
 
-def extractBox(url="https://coronationstreet.fandom.com/",name="Stella_Price"):
+def extractBox(url="https://coronationstreet.fandom.com/",name="Amy_Barlow"):
     #xyz = 'Stella_Price'
+    print(name)
     fullurl = url + name
     resp = requests.get(fullurl, params={'action': 'raw'})
     page = resp.text
 
     data = {}
-
+    json_data = {}
+    
     for line in page.splitlines():
-        if line.startswith('|'):
-                value = line.partition('=')[-1].strip() #value  
-                #process value
-                value = value.replace("[[","") 
-                value = value.replace("]]","") 
-                value = value.replace("}}","") 
-                value = value.replace("{{","") 
-                
-                
-                value = re.sub("([\(\[]).*?([\)\]])", "\g<1>\g<2>", value)
-                value = value.replace("()","") 
 
-                value = value.strip() #remove any training white spaces left
-                ##if we have a br the key becomes an array
-                if("<br>" in value):
-                    value = value.partition('<br>')
-                    value = [x for x in value if x != "<br>"]
-                    value = [x.strip() for x in value]
-                    
-                key = line.partition('=')[0].strip()[1:] #key
-                #process key
-                data[key] = value
-                
-    json_data = json.dumps(data, indent=4, sort_keys=True)
-    print(json_data)
+       if line.startswith('|'):
+              value = line.partition('=')[-1].strip() #value  
+              #process value
+              value = value.replace("[[","") 
+              value = value.replace("]]","") 
+              value = value.replace("}}","") 
+              value = value.replace("{{","") 
+       
+              value = re.sub("([\(\[]).*?([\)\]])", "\g<1>\g<2>", value)
+              value = value.replace("()","") 
 
-    f = open(name + ".json", 'w') 
-    f.write(json_data)
+              value = value.strip() 
+              
+              #remove any training white spaces left
+              ##if we have a br the key becomes an array
+
+              if("<br>" in value):
+                     ##value = value.partition('<br>')
+                     ##value = [x for x in value if x != "<br>"]
+                     ##value = [x.strip() for x in value]
+                     value = value.split('<br>')
+                     value = [x.strip() for x in value]
+
+              key = line.partition('=')[0].strip()[1:] #key
+
+              #process key
+              try:
+                     data[name][key] = value
+              except:
+                     data[name] = {}
+                     data[name][key] = value
+      
+    #json_data = json.dumps(data, indent=4, sort_keys=True)
+
+
+    return(data)
+
+
 
 def extractURLs(fullurl):
     print(fullurl)
@@ -57,22 +70,29 @@ def extractURLs(fullurl):
     except:
         cmcontinue = "1"
 
+       
     return cmcontinue, names
 
 
 if __name__ == "__main__":
 
-    cont = "0"
-    titles = []
+       cont = "0"
+       titles = []
 
-    while cont != "1": 
-        result = extractURLs("https://coronationstreet.fandom.com//api.php?action=query&format=json&list=categorymembers&cmtitle=Category:Coronation_Street_characters&cmlimit=500" + "&cmcontinue=" + cont)
-        cont = result[0]
-        titles = result[1]
-        print(result[1])
-        print(cont)
+       while cont != "1": 
+              result = extractURLs("https://coronationstreet.fandom.com//api.php?action=query&format=json&list=categorymembers&cmtitle=Category:Coronation_Street_characters&cmlimit=500" + "&cmcontinue=" + cont)
+              cont = result[0]
+              titles = result[1]
 
 
-    for title in titles:
-        extractBox(url="https://coronationstreet.fandom.com/",name=title)
-   
+       full_data = {}
+
+       for title in titles:
+              full_data[title] = extractBox(url="https://coronationstreet.fandom.com/",name=title)
+
+
+       json_data = json.dumps(full_data, indent=4, sort_keys=True)
+
+       f = open("json_data.json", 'w') 
+       f.write(json_data)
+
